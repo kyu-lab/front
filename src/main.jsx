c85025f79a94276bb9f25a62134b5d0fd33aa522
Choikyudong <slgmgm@naver.com>
2025-04-15 23:25:11 +0900

    feat: 게시글 목록 구독 버튼 추가 및 userStore 수정
    - 다음과 규칙으로 버튼이 표시
    1. 로그인을 하였을 경우
    2. 자신을 제외한 사용자의 작성글
    
    - userStore의 id를 number로 변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import "./styles/main.css"
 import App from './App.jsx'
 
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react' 개발에 필요시 사용
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,22 +1,26 @@
 import React, {useEffect, useRef, useState} from "react";
 import {Link} from "react-router-dom";
-import {getPosts} from "../../service/postService.js";
+import {deletePost, getPosts} from "../../service/postService.js";
 import Loading from "../../components/Loading.jsx";
 import uiStore from "../../utils/uiStore.js";
 import UserImg from "../../components/UserImg.jsx";
 import {formatRelativeTime} from "../../utils/dateUtils.js";
+import userStore from "../../utils/userStore.js";
+import {promptStatus} from "../../utils/enums.js";
+import {followUser} from "../../service/FollowService.js";
+import PostInfo from "./post/PostInfo.jsx";
 
 export default function Main() {
-  // 페이지 정렬 기준
+  // 목록 제어
   const [order, setOrder] = useState(localStorage.getItem("order") || "N");
 
-  // 사용자 데이터
   const [postSummaryList, setPostSummaryList] = useState([]);
-
   // 스크롤 상태 제어
+
   const nextCursorRef = useRef(null); // 다음 커서 관리
   const hasMoreRef = useRef(true); // 더 불러올 데이터가 있는지
   const observerRef = useRef(null);
+  const loading = useRef(false);
 
   // ui제어
   const {isLoading, openLoading, closeLoading} = uiStore(state => state.loading);
@@ -41,7 +45,7 @@ export default function Main() {
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
-        if (isLoading) {
+        if (loading.current) {
           return;
         }
         if (entries[0].isIntersecting && hasMoreRef.current) {
@@ -65,10 +69,11 @@ export default function Main() {
   }, []);
 
   const handlePosts = async () => {
-    if (!hasMoreRef.current || isLoading) { // 중복 조회 방지
+    if (!hasMoreRef.current || loading.current) { // 중복 조회 방지
       return;
     }
     try {
+      loading.current = true;
       const response = await getPosts(nextCursorRef.current, order);
       setPostSummaryList((prev) => [...prev, ...response.postSummaryList]);
       hasMoreRef.current = response.hasMore;
@@ -76,6 +81,7 @@ export default function Main() {
     } catch (error) {
       console.error(`게시글 로드 실패 : ${error}`);
     } finally {
+      loading.current = false;
     }
   };
 
@@ -107,11 +113,11 @@ export default function Main() {
           .map((e, index) => (
             <div
               key={index}
-              className="py-3"
+              className="py-3 px-10"
             >
               <Link to={`/post/${e.postId}`}>
                 <PostInfo
-                  userInfo={e.usersInfo}
+                  writerInfo={e.writerInfo}
                   createdAt={e.createdAt}
                   subject={e.subject}
                   summary={e.summary}
@@ -136,57 +142,3 @@ export default function Main() {
     </>
   );
 }
-
-const PostInfo = ({userInfo, subject, createdAt, summary, postViewCount, commentCount}) => {
-  return (
-    <div className="p-6 shadow rounded-2xl bg-white dark:bg-gray-800">
-      <div className="flex space-x-3">
-        {/* 작성자 이미지 */}
-        <div className="flex-shrink-0">
-          <UserImg imgUrl={userInfo.imgUrl} />
-        </div>
-        <div className="min-w-0 flex-1">
-          {/* 게시글 작성자 및 작성 시간 */}
-          <div className="flex items-center space-x-1">
-            <span className="font-bold text-gray-900 dark:text-white">{userInfo.name}</span>
-          </div>
-          <div className="flex items-center space-x-1">
-            <span className="font-bold text-gray-900 dark:text-white">{formatRelativeTime(createdAt)}</span>
-          </div>
-          
-          {/* 제목 */}
-          <h1 className="font-bold text-gray-900 text-xl dark:text-white mt-2">{subject}</h1>
-          
-          {/* 요악 본문 */}
-          {summary && <p className="text-gray-900 dark:text-white mt-4">{summary}</p>}
-          
-          {/* 구분선 */}
-          <div className="mt-2 rounded-xl overflow-hidden border border-gray-200" />
-          <div className="flex items-center justify-between mt-3">
-            <div className="flex items-center space-x-1 text-gray-500">
-              <button className="p-1 rounded-full">
-                <svg className="h-5 w-5 hover:text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-                </svg>
-              </button>
-              <span className="text-sm">{commentCount}</span>
-            </div>
-            <div className="flex items-center space-x-1 text-gray-500">
-              <button
-                className="p-1 rounded-full"
-                onClick={e => {
-                  e.preventDefault();
-                  alert('기능 미구현');
-                }}
-              >
-                <svg className="h-5 w-5 hover:text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
